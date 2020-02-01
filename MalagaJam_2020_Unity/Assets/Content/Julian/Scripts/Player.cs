855b0e05c1fbbcbe6e575fc36d769714e42b2a3c
Julian Rijken <rijkenjulian@gmail.com>
2020-02-01 17:49:54 +0100

    Player Animations

--- MalagaJam_2020_Unity/Assets/Content/Julian/Scripts/Player.cs
+++ MalagaJam_2020_Unity/Assets/Content/Julian/Scripts/Player.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using XboxCtrlrInput;
 
-[RequireComponent(typeof(Rigidbody2D)),RequireComponent(typeof(CapsuleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D)),RequireComponent(typeof(CapsuleCollider2D)),RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
 
@@ -29,23 +29,36 @@ public class Player : MonoBehaviour
     [Header("Components")]
     private Rigidbody2D m_rigidbody;
     private CapsuleCollider2D m_collider;
+    private Animator m_animator;
 
 
     private void Awake()
     {
         m_rigidbody = GetComponent<Rigidbody2D>();
         m_collider = GetComponent<CapsuleCollider2D>();
+        m_animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        int animatiorState = 0;
 
-        RaycastHit2D hit = Physics2D.CircleCast(transform.position, (m_collider.size.x / 2f) * m_radius, Vector2.down, m_castDistance, m_groundLayer);
-
+        RaycastHit2D groundHit = Physics2D.CircleCast(transform.position, (m_collider.size.x / 2f) * m_radius, Vector2.down, m_castDistance, m_groundLayer);
         float bottomY = transform.position.y - m_collider.size.y / 2f;
-        float groundDistance = bottomY - hit.point.y;
+        float groundDistance = bottomY - groundHit.point.y;
 
-        if ((XCI.GetButtonDown(XboxButton.A, m_controller) || (Input.GetKeyDown(KeyCode.Space))) && groundDistance <= m_jumpGroundDistance)
+        RaycastHit2D roofHit = Physics2D.CircleCast(transform.position, (m_collider.size.x / 2f) * m_radius, Vector2.up, m_castDistance, m_groundLayer);
+        float topY = transform.position.y + m_collider.size.y / 2f;
+        float roofDistance = roofHit.point.y - topY;
+
+        Debug.Log(roofDistance);
+        if(roofDistance < 0 && roofHit.collider != null)
+        {
+            m_velocity.y = 0;
+        }
+
+
+        if ((XCI.GetButtonDown(XboxButton.A, m_controller) || (Input.GetKeyDown(KeyCode.Space))) && groundDistance <= m_jumpGroundDistance && groundHit.collider != null)
         {
             m_velocity.y = m_jumpForce;
         }
@@ -60,15 +73,31 @@ public class Player : MonoBehaviour
             {
                 m_velocity.y -= m_gravity * Time.deltaTime;
                 m_grounded = false;
+                animatiorState = 2;
             }
         }
 
         float move;
+
 #if UNITY_EDITOR
         move = Input.GetAxisRaw("Horizontal") * m_maxSpeed;
 #else
         move = XCI.GetAxis(XboxAxis.LeftStickX, m_controller) * m_maxSpeed;
 #endif
+
+        if (animatiorState != 2)
+        {
+            if (Mathf.Abs(move) > 0f)
+            {
+                animatiorState = 1;
+            }
+            else
+            {
+                animatiorState = 0;
+            }
+        }
+
+        m_animator.SetInteger("state", animatiorState);
 
         float celleration = (Mathf.Abs(move) <= 0.01f ? m_deceleration : m_acceleration);
         celleration *= m_grounded ? m_groundCellerationMultilier : m_airCellerationMultilier;
