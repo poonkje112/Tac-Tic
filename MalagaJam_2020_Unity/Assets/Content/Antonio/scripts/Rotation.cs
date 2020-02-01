08b588c6fc2cf7beeafc75fdaecb75d2b7c43179
Antonio Espino <antonioespinom@gmail.com>
2020-02-01 18:01:11 +0100

    rotation scritp
    if negative is going to clockwise else anticlockwise

---
+++ MalagaJam_2020_Unity/Assets/Content/Antonio/scripts/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    // Start is called before the first frame update  
+    public float speed = 1;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+        transform.eulerAngles += Vector3.forward * speed;
+
+    
+    }
+}
