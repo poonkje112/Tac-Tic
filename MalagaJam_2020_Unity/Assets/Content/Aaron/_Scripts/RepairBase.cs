ab7ea93b0638c32d188b92975c657341dfb56ced
poonkje112 <aaron.knoop@live.nl>
2020-02-01 11:24:51 +0100

    Added (multi) button behaviour

--- MalagaJam_2020_Unity/Assets/Content/Aaron/_Scripts/RepairBase.cs
+++ MalagaJam_2020_Unity/Assets/Content/Aaron/_Scripts/RepairBase.cs
@@ -1,35 +1,60 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using XboxCtrlrInput;
 
 namespace MalagaJam.Object
 {
+    enum ObjectRepairState
+    {
+        Destroyed,
+        Repairing,
+        Repaired
+    }
+
     public class RepairBase : MonoBehaviour, IRepairable
     {
-        List<GameObject> _ObjectsInRange = new List<GameObject>();
+        /** TODO
+         * Set the list to use the player object instead of a GameObject
+         * When the player is done, grab the player controller id
+         * Set the repairing to go on a timer?
+         * 
+         */
 
-        void Start()
-        {
-        }
+        [Header("Object Settings")] [SerializeField]
+        XboxButton repairButton; // Move this to a general Input script?
+        [Space] [Header("DEBUG")] [SerializeField]
+        ObjectRepairState objectRepairState;
 
-        void Update()
+        protected readonly List<GameObject> ObjectsInRange = new List<GameObject>();
+
+        protected virtual void Update()
         {
+            if (Input.GetKeyDown(KeyCode.Space) || XCI.GetButtonDown(repairButton, XboxController.Any))
+            {
+                Repair();
+            }
         }
 
         void OnTriggerEnter2D(Collider2D other)
         {
-            _ObjectsInRange.Add(other.gameObject);
-            print(_ObjectsInRange.Count);
+            ObjectsInRange.Add(other.gameObject);
+            print(ObjectsInRange.Count);
         }
 
         void OnTriggerExit2D(Collider2D other)
         {
-            _ObjectsInRange.Remove(other.gameObject);
-            print(_ObjectsInRange.Count);
+            ObjectsInRange.Remove(other.gameObject);
+            print(ObjectsInRange.Count);
         }
 
-        public void Repair()
+        public virtual void Repair()
         {
+            if (objectRepairState != ObjectRepairState.Destroyed)
+                return; // When the object is repaired is is being repaired we simply return this function
+
+            objectRepairState = ObjectRepairState.Repairing;
+            objectRepairState = ObjectRepairState.Repaired;
         }
     }
 }
